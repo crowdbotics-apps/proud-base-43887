978421ee10021171b59d55098b398731643a28ce
Crowdbotics <team@crowdbotics.com>
2023-10-11 22:31:58 +0000

    Initial commit

---
+++ backend/proud_base_43887/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for proud_base_43887 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "proud_base_43887.settings"
+)
+
+application = get_wsgi_application()
